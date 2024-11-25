d669f4812516aa7aeb14e36a2a5034f28e145a53
BadrLaklach <b.laklach@edu.umi.ac.ma>
2024-11-25 22:59:09 +0100

    first

---
+++ src/main/java/com/ensam/hotelalrbadr/HotelAlRbadrApplication.java
@@ -0,0 +1,13 @@
+package com.ensam.hotelalrbadr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAlRbadrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelAlRbadrApplication.class, args);
+    }
+
+}
